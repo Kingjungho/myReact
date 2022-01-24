f7df103e0d2b839ea7d30906949b698436ff9deb
Kingjungho <dkdlwo@naver.com>
2022-01-24 23:36:57 +0900

    2022.01.24

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,18 +1,38 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useState } from 'react/cjs/react.development'
+import useFetch from '../hooks/useFetch'
 import useFetch from '../hooks/useFetch'
 
 const DayList = ({ list }) => {
-  const days = useFetch("http://localhost:3001/days")
-  
+  const day = useParams();
+  const days = useFetch("http://localhost:3002/days")
+  const [dayList, setDayList] = useState(days.id)
+
+  const deleteDay = () => {
+    if (window.confirm('진짜 삭제?')) {
+      fetch(`http://localhost:3002/words?day=${day}`, {
+        method: 'DELETE',
+      }).then(res => {
+        if (res.ok) {
+          setDayList({ day: 0 })
+        }
+      })
+    }
+  }
+  if(dayList === 0){
+    return null;
+  }
   return (
     <>
       <ul className={list}>
         {days.map(day => (
           <li key={day.id}>
+            <input type="checkbox" />
             <Link to={`/day/${day.day}`}>Day {day.day}</Link>
           </li>
         ))}
       </ul>
+      <button onClick={deleteDay}>Delete</button>
     </>
   )
 }
